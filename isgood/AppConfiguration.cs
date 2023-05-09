baef1c5c5132f24368183da88a2eec4bd0420e03
Hendrik <hendrik@hr94.de>
2023-05-09 23:12:29 +0200

    update config check and values

--- isgood/AppConfiguration.cs
+++ isgood/AppConfiguration.cs
@@ -3,11 +3,16 @@ namespace isgood;
 internal class AppConfiguration {
     public string? MqttBrokerURL { get; set; }
     public int? MqttBrokerPort { get; set; }
+    public bool? MqttBrokerAuthEnabled { get; set; }
     public string? MqttBrokerUsername { get; set; }
     public string? MqttBrokerPassword { get; set; }
-    public string? MqttTopicBarcode { get; set; }
-    public string? MqttTopicBestBeforeSet { get; set; }
-    public int? MqttBestBeforeTimeout { get; set; }
+    public string MqttTopicBarcode { get; set; }
+    public string MqttTopicBestBeforeSet { get; set; }
+    public int MqttBestBeforeTimeout { get; set; }
 
-    public AppConfiguration() {}
+    public AppConfiguration() {
+        MqttTopicBarcode = "product/barcode";
+        MqttTopicBestBeforeSet = "product/best_before/set";
+        MqttBestBeforeTimeout = 15;
+    }
 }
--- isgood/Program.cs
+++ isgood/Program.cs
@@ -16,7 +16,8 @@ class Program
         ");
         Console.WriteLine("+ Starting isgood");
 
-        try {
+        try 
+        {
             var builder = new ConfigurationBuilder();
 			builder
 				.SetBasePath(Directory.GetCurrentDirectory())
@@ -26,20 +27,36 @@ class Program
 			_appConfiguration = new();
             configuration.Bind(_appConfiguration);
 
-            if (_appConfiguration.MqttBrokerURL is null || _appConfiguration.MqttBrokerURL == string.Empty) {
+            if (_appConfiguration.MqttBrokerURL is null || _appConfiguration.MqttBrokerURL == string.Empty) 
+            {
                 throw new Exception("Configuration file is missing value 'MqttBrokerURL'");
             }
-            else if (_appConfiguration.MqttBrokerPort is null || _appConfiguration.MqttBrokerPort < 0 ) {
+            else if (_appConfiguration.MqttBrokerPort is null || _appConfiguration.MqttBrokerPort < 0 ) 
+            {
                 throw new Exception("Configuration file is missing value 'MqttBrokerPort'");
             }
+            else if (_appConfiguration.MqttBrokerAuthEnabled is not null) 
+            {
+                if (_appConfiguration.MqttBrokerAuthEnabled == true) 
+                {
+                    if (_appConfiguration.MqttBrokerUsername is null || _appConfiguration.MqttBrokerUsername == string.Empty)
+                    {
+                        throw new Exception("Configuration file is missing value 'MqttBrokerUsername'");
+                    }
+                    else if (_appConfiguration.MqttBrokerPassword is null || _appConfiguration.MqttBrokerPassword == string.Empty)
+                    {
+                        throw new Exception("Configuration file is missing value 'MqttBrokerPassword'");
+                    }
+                }
+            }
         }
-        catch (Exception ex) {
+        catch (Exception ex) 
+        {
             Console.WriteLine($"+ ERROR: {ex.Message}");
             System.Environment.Exit(1);
         }
 
         string brokerUrl = $"{_appConfiguration.MqttBrokerURL}:{_appConfiguration.MqttBrokerPort}";
-
         Console.WriteLine($"+ Connecting to mqtt broker '{brokerUrl}' ...");
     }
 }
