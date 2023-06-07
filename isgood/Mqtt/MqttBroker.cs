2065ac8fa3f45d7a39adcd0e67641fc217e43d60
Hendrik <hendrik@hr94.de>
2023-06-07 23:26:46 +0200

    Move folder

--- isgood/Mqtt/MqttBroker.cs
+++ isgood/Mqtt/MqttBroker.cs
@@ -19,9 +19,9 @@ namespace isgood.Mqtt;
 public class MqttBroker
 {
     private readonly MqttServer mqttBroker;
-    private readonly MqttConfiguration _mqttConfiguration;
+    private MqttConfiguration _mqttConfiguration;
     private Timer? bestBeforeTimeout;
-    private readonly List<Product> Products;
+    private List<Product> Products;
 
     public MqttBroker(MqttConfiguration mqttConfiguration)
     {
