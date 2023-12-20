be988674c699caf6e95c5e8c0ef1316b31768ac3
Hendrik <hendrik@hr94.de>
2023-12-20 22:09:17 +0100

    update CRUD methods

--- backend/handlers/v1/product.go
+++ backend/handlers/v1/product.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"isgood/backend/models"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -19,10 +20,33 @@ func GetProducts(c *gin.Context) {
 	var products []models.Product
 	db.Find(&products)
 
-	c.JSON(http.StatusOK, products)
+	c.IndentedJSON(http.StatusOK, products)
 }
 
-func GetProduct(c *gin.Context) {}
+func GetProduct(c *gin.Context) {
+	id := c.Param("id")
+
+	if _, err := strconv.Atoi(id); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("ID '%s' is invalid", id)})
+		return
+	}
+
+	db, ok := c.MustGet("db").(*gorm.DB)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get database from context"})
+		return
+	}
+
+	var product models.Product
+	getError := db.First(&product, id)
+
+	if getError.Error != nil || product.ID <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Product with id '%s' was not found", id)})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, product)
+}
 
 func CreateProduct(c *gin.Context) {
 	db, ok := c.MustGet("db").(*gorm.DB)
@@ -52,6 +76,75 @@ func CreateProduct(c *gin.Context) {
 	c.JSON(http.StatusCreated, product)
 }
 
-func UpdateProduct(c *gin.Context) {}
+func UpdateProduct(c *gin.Context) {
+	id := c.Param("id")
 
-func DeleteProduct(c *gin.Context) {}
+	if _, err := strconv.Atoi(id); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("ID '%s' is invalid", id)})
+		return
+	}
+
+	db, ok := c.MustGet("db").(*gorm.DB)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get database from context"})
+		return
+	}
+
+	var product models.Product
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println(err.Error())
+		return
+	}
+
+	if product.Barcode == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "barcode missing"})
+		return
+	}
+
+	var dbProduct models.Product
+	getError := db.First(&product, id)
+
+	dbProduct.Barcode = product.Barcode
+	dbProduct.ProductName = product.ProductName
+	dbProduct.Categories = product.Categories
+	dbProduct.Countries = product.Countries
+	dbProduct.ImageURL = product.ImageURL
+	dbProduct.BestBefore = product.BestBefore
+
+	if getError.Error != nil || product.ID <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Product with id '%s' was not found", id)})
+		return
+	}
+
+	saveResult := db.Save(&product)
+	if saveResult.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": saveResult.Error.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, product)
+}
+
+func DeleteProduct(c *gin.Context) {
+	id := c.Param("id")
+
+	if _, err := strconv.Atoi(id); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("ID '%s' is invalid", id)})
+		return
+	}
+
+	db, ok := c.MustGet("db").(*gorm.DB)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get database from context"})
+		return
+	}
+
+	deleteResult := db.Delete(&models.Product{}, id)
+	if deleteResult.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": deleteResult.Error.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"success": fmt.Sprintf("Product with ID '%s' was deleted", id)})
+}
